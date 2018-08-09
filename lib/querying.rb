91b127fb9ace32a67262a3cb3c424d7949783562
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-08-09 03:33:37 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -4,7 +4,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
   JOIN books
   ON series.id = books.series_id
   WHERE series_id = 1
-  ORDER BY year;"
+  ORDER BY year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
@@ -16,11 +16,21 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species)
+  FROM characters
+  GROUP BY species
+  ORDER BY COUNT(*) DESC
+  LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+  FROM authors
+  JOIN series, subgenres
+  ON authors.id = subgenres.id
+  GROUP BY authors.name
+  ORDER BY subgenres.name
+  LIMIT 2"
 end
 
 def select_series_title_with_most_human_characters
